5154045bc6524b21a7f6956d1334989d74e1c4c8
joewachira <joewachira49@gmail.com>
2017-11-23 19:43:13 +0300

    update test file

--- tests/test_events.py
+++ tests/test_events.py
@@ -1,21 +1,21 @@
 __author__ = 'joe'
 
 import unittest
-from app.events_operations import Events
+from app.events_operations import EventsManager
 
 
 class TestEvent(unittest.TestCase):
 	def setUp(self):
-		self.event = Events()
+		self.event = EventsManager()
 		# self.items = dict(category="hackathon", name="rubics cube", time="11:45pm", location="galana plaza")
 
 		# checks method types in adding an event
 
-		def test_add_event(self):
+	def test_add_event_if_key_anInt(self):
 			self.assertRaises(ValueError, self.event.add_event, 4, "number")
 
-		def test_update(self):
-			self.assertRaises
+	def test_update_event(self):
+			self.assertRaises(ValueError, self.event.update_event())
 
-
-
+	def test_get_event_list_for_nonInt(self):
+		self.assertRaises(ValueError, self.event.get_event_list(2, 'Night Dance'))
